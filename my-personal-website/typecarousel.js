2c91e19fff55ffa0f64e13530374c318028a2292
evapunkt <103138496+evapunkt@users.noreply.github.com>
2022-04-23 15:46:27 +0200

    Add files via upload

---
+++ my-personal-website/typecarousel.js
@@ -0,0 +1,33 @@
+let words = ['a cookie.', 'my best.', 'sometimes maybe good.', 'sometimes maybe shit.']
+let index = 0;
+let deleting = false; 
+let text = '';
+let word = words[index];
+let time = 500;
+
+window.onload = function() {
+    time = deleting ? 300 : 400;
+    let carousel = document.getElementById("carousel");
+    carousel.innerHTML = text; 
+
+    if (deleting) {
+        if(text.length == 0) {
+            deleting = false;
+            index = index + 1;
+            word = words[index % words.length]
+        } else {
+            text = text.substring(0, text.length - 1);
+        }
+    } else {
+        if(text.length == word.length) {
+            deleting = true;
+            time = 200;
+        } else {
+            text = text + word.charAt(text.length);
+        }
+    }
+
+    setTimeout(function() {
+        window.onload();
+    }, time);
+}
